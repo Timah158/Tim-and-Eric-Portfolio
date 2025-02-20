2286356f41064338880c3c123649d6b87a73cb86
Timah158 <timothylopez95@gmail.com>
2025-02-19 20:42:48 -0600

    Added resume download API and fixed profile contact buttons.

--- src/app/eric/page.tsx
+++ src/app/eric/page.tsx
@@ -1,9 +1,43 @@
-import Link from "next/link.js";
+'use client'
+
+import { useState } from "react";
+import axios from 'axios';
+import { useModal } from "@/app/context/modalContext";
 import EricSkills from "@/app/SVGs/EricSkills.svg"
 import '@/app/CSS/profile.modules.css'
 import "../welcome.modules.css"
 
 export default function EricProfile() {
+  const { toggleModal, updateModal } = useModal();
+  
+  const [downloadStatus, setDownloadStatus] = useState<string>("");
+  const downloadResume = async (fileName: string) => {
+    try {
+        const response = await axios.get(`/api/download?file=${fileName}`, {
+            responseType: "blob", // Important for binary data
+        });
+
+        // Extract filename from content-disposition header
+        const contentDisposition = response.headers["content-disposition"];
+        const fileNameMatch = contentDisposition.match(/filename="(.+)"/);
+        const downloadedFileName = fileNameMatch ? fileNameMatch[1] : "downloadedFile.pdf";
+
+        // Create a temporary anchor element to trigger the download
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        // Setting filename received in response
+        link.setAttribute("download", downloadedFileName);
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+
+        setDownloadStatus("Downloaded");
+      } catch (error) {
+          console.error("Error downloading file:", error);
+          setDownloadStatus("Error downloading");
+    }
+  };
   return (
     <main className="text-[color:var(--primary-font)] font-Ununtu">
       <div id="section_1" className="section">
@@ -19,8 +53,8 @@ export default function EricProfile() {
             </p>
           </div>
           <div id="main_title_buttons">
-            <button className="main_button">Contact</button>
-            <Link href="/api/download/ericResume.pdf" className="main_button">Resume</Link>
+            <button className="main_button" onClick={() => updateModal(true)}>Contact</button>
+            <button className='main_button' onClick={() => downloadResume("ericResume.pdf")}>Resume</button>
           </div>
         </section>
       </div>
