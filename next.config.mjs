93425dfb4531d485cc999a92e27ec2a55dc33aa4
Eric Moore <37810345+emoore36@users.noreply.github.com>
2025-12-05 08:06:41 +0000

    removed webpack/turbopack config in nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    turbopack(config) {
-        config.module.rules.push({
-          test: /\.svg$/,
-          use: ["@svgr/turbopack"],
-        });
-    
-        return config;
-      },
+
 };
 
 export default nextConfig;
